e6b75e7dc52d2ee1ba5c17348ded55fe081b6c40
PhilWilky <phil@philwilky.me>
2018-12-17 00:16:00 +0000

    Lecture 29 - Finished Code

--- Assets/Hacker.cs
+++ Assets/Hacker.cs
@@ -3,8 +3,11 @@
 public class Hacker : MonoBehaviour {
 
     // Game config data
-    string[] level1Passwords = { "Education","Teachers","curriculum","syllabus"};
-    string[] level2Passwords = { "Convict", "Prisoner", "Manslaughter", "Homicide" };
+    const string menuHint = "You may type menu at any time";
+
+    string[] level1Passwords = { "education","teachers","curriculum","syllabus"};
+    string[] level2Passwords = { "convict", "prisoner", "manslaughter", "homicide" };
+    string[] level3Passwords = { "convict", "prisoner", "manslaughter", "homicide" };
 
     // Game state
     int level;
@@ -40,7 +43,8 @@ public class Hacker : MonoBehaviour {
         Terminal.WriteLine("");
         Terminal.WriteLine("Press 1 for local School");
         Terminal.WriteLine("Press 2 for Police Station");
-        Terminal.WriteLine("Enter your selection:");
+        Terminal.WriteLine("Press 3 for City Hall");
+        Terminal.WriteLine("Enter your selection:...");
 
 
     }
@@ -65,47 +69,111 @@ public class Hacker : MonoBehaviour {
 
     void RunMainMenu(string input)
     {
-        bool isvalidLevelNumber = (input == "1" || input == "2");
+        bool isvalidLevelNumber = (input == "1" || input == "2" || input == "3");
         if (isvalidLevelNumber)
         {
             level = int.Parse(input);
-            StartGame();
+            AskForPassword();
         }
         else
         {
             Terminal.WriteLine("Command unknown");
+            Terminal.WriteLine(menuHint);
         }
     }
 
-    void StartGame()
+    void AskForPassword()
     {
         currentScreen = Screen.Password;
         Terminal.ClearScreen();
+        SetRandomPassword();
+
+        Terminal.WriteLine("Enter your Password, hint:" + password.Anagram());
+        Terminal.WriteLine(menuHint);
+    }
+
+    void SetRandomPassword()
+    {
         switch (level)
         {
             case 1:
                 password = level1Passwords[Random.Range(0, level1Passwords.Length)];
                 break;
             case 2:
-                password = level1Passwords[Random.Range(0, level2Passwords.Length)];
+                password = level2Passwords[Random.Range(0, level2Passwords.Length)];
                 break;
             default:
                 Debug.LogError("Invailed Level");
                 break;
+            case 3:
+                password = level3Passwords[Random.Range(0, level3Passwords.Length)];
+                break;
         }
-
-        Terminal.WriteLine("Please enter your Password");
     }
 
     void CheckPassword(string input)
     {
         if (input == password)
         {
-            Terminal.WriteLine("Well done!");
+            DisplayWinScreen();
         }
         else
         {
-            Terminal.WriteLine("Sorry, wrong password!");
+            AskForPassword();
+        }
+    }
+
+    void DisplayWinScreen()
+    {
+        currentScreen = Screen.Win;
+        Terminal.ClearScreen();
+        ShowLevelReward();
+        Terminal.WriteLine(menuHint);
+    }
+
+    void ShowLevelReward()
+    {
+        switch(level)
+        {
+            case 1:
+                Terminal.WriteLine("Have a Book...");
+                Terminal.WriteLine(@"
+    ______
+   /     //
+  /     //
+ /_____//
+(_____(/
+"                                   );
+                break;
+            case 2:
+                Terminal.WriteLine("You found some handcuffs!");
+                Terminal.WriteLine(@"
+                           
+      .::::::::::.                          .::::::::::.
+    .::::''''''::::.                      .::::''''''::::.
+  .:::'          `::::....          ....::::'          `:::.
+ .::'             `:::::::|        |:::::::'             `::.
+.::|               |::::::|_ ___ __|::::::|               |::.
+`--'               |::::::|_()__()_|::::::|               `--'
+ :::               |::-o::|        |::o-::|               :::
+ `::.             .|::::::|        |::::::|.             .::'
+  `:::.          .::\-----'        `-----/::.          .:::'
+    `::::......::::'                      `::::......::::'
+      `::::::::::'                          `::::::::::'
+");
+                break;
+            case 3:
+                Terminal.WriteLine("You got the Key to the City!!!!");
+                Terminal.WriteLine("YOU HAVE WON THE GAME, WELL DONE");
+                Terminal.WriteLine(@"
+ _
+/0 \_______
+\__/-=' = '
+");
+                break;
+            default:
+                Debug.LogError("Invalid level reached");
+                break;
         }
     }
 }
